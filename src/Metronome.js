3f34e0f405693d1025830df9820ce45e19e63393
tb <tbussanich@gmail.com>
2018-10-16 13:26:23 -0700

    initialize state

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import './Metronome.css';
 
 class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4,
+    };
+  }
+
   render() {
-    let bpm = 100;
-    let playing = false;
+    const { playing, bpm } = this.state;
 
     return (
       <div className="metronome">
